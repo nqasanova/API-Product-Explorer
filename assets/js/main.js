f038f67e29f991f6dde64687581753d225456983
Natavan Hasanova <natavanhesenova4@gmail.com>
2023-11-29 23:12:37 +0400

    Implement JavaScript code to render detailed information about each product in a visually appealing format on the home web page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,64 @@
 fetch('https://dummyjson.com/products')
-    .then(response => {
-        // Check if the response status is OK (200)
+    .then((response) => {
+        // Check if the response status is OK
         if (!response.ok) {
             throw new Error(`Network response was not ok (Status: ${response.status})`);
         }
-        
-        // Parse the JSON data from the response
         return response.json();
     })
-    .then(data => {
-        // Handle the retrieved data (e.g., display it on the console)
-        console.log('Products data:', data);
+    .then((completedata) => {
+        console.log('Response Data:', completedata); // Log the response data to the console
+
+        // Check if "products" key exists in the response
+        if (completedata.products) {
+            // Your existing code for processing the data
+            let data1 = "";
+            completedata.products.map((products) => {
+                data1 += ` <div class="card">
+                    <img src=${products.thumbnail} alt="img" class="images">
+
+                    <div class="brand-info">
+                        <h1 class="brand-label">Brand:</h1>
+                        <h1 class="brand-name">${products.brand}</h1>
+                    </div>
+
+                    <div class="brand-info">
+                        <h1 class="title-label">Title:</h1>
+                        <h1 class="title-name">${products.title}</h1>
+                    </div>
+
+                    <h4 class="description">${products.description}<h4>
+
+                    <div class="brand-info">
+                        <h1 class="category-label">Category:</h1>
+                        <h1 class="category-name">${products.category}</h1>
+                    </div>
+
+                    <div class="brand-info">
+                        <h1 class="stock-label">Stock:</h1>
+                        <h1 class="stock-name">${products.stock}</h1>
+                     </div>
+
+                     <div class="brand-info">
+                        <h1 class="price-label">Price:</h1>
+                        <h1 class="price-name">${products.price}</h1>
+                    </div>
+
+                    <div class="brand-info">
+                        <h1 class="rating-label">Rating:</h1>
+                        <h1 class="rating-name">${products.rating}</h1>
+                    </div>
+
+                    <div class="brand-info">
+                        <h1 class="discount-label">Discount:</h1>
+                        <h1 class="discount-name">${products.discountPercentage}</h1>
+                    </div>
+
+                </div>`;
+            });
+            document.getElementById("cards").innerHTML = data1;
+        }
     })
     .catch(error => {
-        // Handle errors during the fetch operation
-        console.error('Error during fetch operation:', error.message);
+        console.log('An error occured during fetch operation:', error);
     });
